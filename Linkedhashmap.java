6449106e88ad063b7e28255156aebd42284a0c89
VAIBHAV~G <88087669+btech10423@users.noreply.github.com>
2023-02-04 15:52:02 +0530

    Update Linkedhashmap.java

--- Linkedhashmap.java
+++ Linkedhashmap.java
@@ -2,6 +2,11 @@ import java.util.*;
 
 public class Linkedhashmap {
     public static void main(String args[]) {
+        LinkedHashMap<String, Integer> lhm = new LinkedHashMap<>();
+        lhm.put("India", 100);
+        lhm.put("China", 150);
+        lhm.put("US", 50);
+        
         TreeMap<String, Integer> tm = new TreeMap<>();
         tm.put("India", 100);
         tm.put("China", 150);
